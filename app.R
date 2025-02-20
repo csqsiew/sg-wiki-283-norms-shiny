5c50329b219a917fa7ddc1d9bd6e7dce0dee0b24
Cynthia Siew <cynthia.siew@mailfence.com>
2025-02-20 16:38:06 +0800

    second commit

--- app.R
+++ app.R
@@ -1,15 +1,7 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
-library(tidyverse)
 library(ggplot2)
+library(tidyverse)
+library(markdown)
 
 test_data <- read.csv('data-for-app.csv') |> 
   rename(Valence = mean_valence,
@@ -21,8 +13,194 @@ test_data <- read.csv('data-for-app.csv') |>
          Concreteness = round(Concreteness, 2),
          Humor = round(Humor, 2))
 
-# Define server logic required to draw a histogram
-function(input, output, session) {
+ui <- navbarPage("Singlish norms",
+           
+            ### ABOUT ----
+  tabPanel('About', 
+           fluidRow(
+             column(6,
+                    includeMarkdown("about.md")
+          )
+        )
+      ),
+           ### TABLE ### 
+            tabPanel("Table",
+                    titlePanel("Lexical-semantic norms for 283 Singlish Concepts"),
+  
+  # Create a new Row in the UI for selectInputs
+  fluidRow(
+    column(4,
+           selectInput("man",
+                       "Item:",
+                       c("All",
+                         unique(as.character(test_data$Word))))
+    ),
+  ),
+  # Create a new row for the table.
+  DT::dataTableOutput("table"),
+  downloadButton("downloadData", "Download Data")
+                    
+           ),
+  
+  ### VALENCE ---- 
+  tabPanel("Valence",
+               
+               # Give the page a title
+  titlePanel("Valence norms"),
+  
+  # Generate a row with a sidebar
+  sidebarLayout(      
+    
+    # Define the sidebar with one input
+    sidebarPanel(
+      # input some words 
+      tags$h4('Select up to 20 items from the list.'),
+      selectizeInput(
+        'fooV', label = '', choices = c("", test_data$Word), multiple = TRUE, 
+        options = list(maxItems = 20, placeholder = 'search for a Singlish word!')
+      ),
+      br(),
+      # filter range 
+      sliderInput( 
+        "sliderV", "", 
+        min = 1, max = 9, 
+        value = c(1, 9), step = 0.5 
+      ), 
+      hr(),
+      helpText("Valence refers to the pleasantness of the emotion elicited by a particular word or concept. It ranges from highly pleasant (e.g., joke and kitten) to unpleasant (e.g., jail and kill). The Singlish items were rated on a 9 point Likert scale, with 1 = most unpleasant (negative) and 9 = most pleasant (positive).")
+    ),
+    
+    # Create a spot for the barplot
+    mainPanel(
+      fluidRow(
+        column(9,
+      plotOutput("distPlotValence", height = 900)  
+    )
+    )
+    )
+  )
+  ),
+  
+  ### AROUSAL ---- 
+  tabPanel("Arousal",
+           
+           # Give the page a title
+           titlePanel("Arousal norms"),
+           
+           # Generate a row with a sidebar
+           sidebarLayout(      
+             
+             # Define the sidebar with one input
+             sidebarPanel(
+               # input some words 
+               tags$h4('Select up to 20 items from the list.'),
+               selectizeInput(
+                 'fooA', label = '', choices = c("", test_data$Word), multiple = TRUE, 
+                 options = list(maxItems = 20, placeholder = 'search for a Singlish word!')
+               ),
+               br(),
+               # filter range 
+               sliderInput( 
+                 "sliderA", "", 
+                 min = 1, max = 9, 
+                 value = c(1, 9), step = 0.5 
+               ), 
+               hr(),
+               helpText("Arousal refers to the degree of arousal invoked by a particular word or concept. It ranges from highly arousing (e.g., sex and thrill) to unexciting (e.g., statue and bored). The Singlish items were rated on a 9 point Likert scale, with 1 = least arousing (boring) and 9 = highly arousing (exciting).")
+             ),
+             
+             # Create a spot for the barplot
+             mainPanel(
+               fluidRow(
+                 column(9,
+                        plotOutput("distPlotArousal", height = 900)  
+                 )
+               )
+             )
+           )
+  ),
+  
+  ### CONCRETENESS ----
+  tabPanel("Concreteness",
+           
+           # Give the page a title
+           titlePanel("Concreteness norms"),
+           
+           # Generate a row with a sidebar
+           sidebarLayout(      
+             
+             # Define the sidebar with one input
+             sidebarPanel(
+               # input some words 
+               tags$h4('Select up to 20 items from the list.'),
+               selectizeInput(
+                 'fooC', label = '', choices = c("", test_data$Word), multiple = TRUE, 
+                 options = list(maxItems = 20, placeholder = 'search for a Singlish word!')
+               ),
+               br(),
+               # filter range 
+               sliderInput( 
+                 "sliderC", "", 
+                 min = 1, max = 5, 
+                 value = c(1, 5), step = 0.5 
+               ), 
+               hr(),
+               helpText("Concreteness refers to the extent to which the concept represented by a word (or short phrase) refers to a perceptible entity. It ranges from highly concrete, whereby an entity exists in the real world and elicits immediate experiences of that concept through one’s senses, to highly abstract, whereby an entity cannot be directly experienced through one’s senses. The Singlish items were rated on a 5 point Likert scale, with 1 = most abstract and 5 = most concrete.")
+             ),
+             
+             # Create a spot for the barplot
+             mainPanel(
+               fluidRow(
+                 column(9,
+                        plotOutput("distPlotConcreteness", height = 900)  
+                 )
+               )
+             )
+           )
+  ),
+  
+  ### HUMOR ----
+  tabPanel("Humor",
+           
+           # Give the page a title
+           titlePanel("Humor norms"),
+           
+           # Generate a row with a sidebar
+           sidebarLayout(      
+             
+             # Define the sidebar with one input
+             sidebarPanel(
+               # input some words 
+               tags$h4('Select up to 20 items from the list.'),
+               selectizeInput(
+                 'fooH', label = '', choices = c("", test_data$Word), multiple = TRUE, 
+                 options = list(maxItems = 20, placeholder = 'search for a Singlish word!')
+               ),
+               br(),
+               # filter range 
+               sliderInput( 
+                 "sliderH", "", 
+                 min = 1, max = 5, 
+                 value = c(1, 5), step = 0.5 
+               ), 
+               hr(),
+               helpText("Humor ratings capture the extent to which a word or concept elicited feelings of amusement or humorous thought. It ranges from highly humorous (e.g., booty and waddle) to unhumorous (e.g., nightmare and pain). The Singlish items were rated on a 5 point Likert scale, with 1 = most humorless and 5 = most humorous.")
+             ),
+             
+             # Create a spot for the barplot
+             mainPanel(
+               fluidRow(
+                 column(9,
+                        plotOutput("distPlotHumor", height = 900)  
+                 )
+               )
+             )
+           )
+  )
+
+)
+
+server <- function(input, output, session) {
   
 # Filter data based on selections
   output$table <- DT::renderDataTable(DT::datatable({
@@ -196,3 +374,6 @@ sample_data_conc <- reactive({
     }) 
   
 }
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ readme.md
@@ -0,0 +1,19 @@
+# R Shiny Application to Explore Singlish Norms 
+
+This application is an accompaniment to our research developing lexical-semantic norms for Singapore English. It features the definitions, valence, arousal, concreteness, and humor of 283 common Singlish words and concepts. 
+
+Feel free to pull this repo and run the R Shiny application locally in your computer. Otherwise, you can also view a demo of this application [here](). 
+
+More information about the methodology involved in developing the data featured in this application can be found in our [preprint](https://osf.io/bjxfa/), and the raw data can be downloaded in the 'Table' tab or at our [OSF page](https://osf.io/bjxfa/).
+
+## Keep in touch
+
+If you would like to receive updates about the project, drop us an email at [singlishwords@nus.edu.sg](mailto:singlishwords.nus.edu.sg), or check out our social media pages below:
+
+[Facebook](https://fb.me/singlishwords) | [Twitter](https://twitter.com/singlishwords) | [Project Page](https://langcomplab.github.io/singlish.html)
+
+#### Acknowledgements
+
+We would like to thank **[X]** for helping us process the definitions featured on this visualization application. 
+
+Our research is funded by the Social Science Research Council of Singapore (MOE 2021-SSHR-003; Project title: "Measuring the Singaporean Mental Lexicon: Lexical-semantic norms for Singapore English words").
